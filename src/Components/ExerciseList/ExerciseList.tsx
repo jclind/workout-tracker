ab464ff2733e88eef9ae9108a6a4059966a84050
Jesse Lind <Jesseclind@gmail.com>
2023-07-17 16:00:40 -0400

    feat: add exercise input focus / add / deletion

--- src/Components/ExerciseList/ExerciseList.tsx
+++ src/Components/ExerciseList/ExerciseList.tsx
@@ -1,59 +1,134 @@
-import React, { useState } from 'react'
+import React, { RefObject, useEffect, useRef, useState } from 'react'
 import './ExerciseList.scss'
 import { parseExerciseData } from '../../util/parseExerciseData'
 import { AiFillInfoCircle, AiOutlinePlusCircle } from 'react-icons/ai'
 import InfoModal from '../InfoModal/InfoModal'
 import FormInput from '../FormInput/FormInput'
+import { v4 as uuidv4 } from 'uuid'
 
 type ExerciseInputsProps = {
   exercise: ExerciseType
   idx: number
+  numExercises: number
+  exercises: ExerciseType[]
+  addExercise: () => string
+  removeExercise: (id: string) => void
 }
-const ExerciseInputs = ({ exercise, idx }: ExerciseInputsProps) => {
+
+const test: { [x: string]: React.RefObject<HTMLInputElement> } = {}
+
+const ExerciseInputs = ({
+  exercise,
+  idx,
+  numExercises,
+  exercises,
+  addExercise,
+  removeExercise,
+}: ExerciseInputsProps) => {
   const [name, setName] = useState('')
   const [data, setData] = useState('')
 
+  const nextID = exercises[idx + 1]?.id || null
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (exercise.id) {
+      test[exercise.id] = inputRef
+    }
+  }, [])
+
+  const handleEnter = (id: string | null) => {
+    console.log('Curr:', exercise.id, 'Next:', id)
+    if (id) {
+      const focusRef = test[id]
+      console.log(test)
+      console.log(focusRef)
+      console.log(id)
+      if (id && focusRef && focusRef.current) {
+        focusRef.current.focus()
+      }
+    } else {
+      const nextID = addExercise()
+      setTimeout(() => {
+        handleEnter(nextID)
+      }, 0)
+    }
+  }
+
+  const handleBackspace = () => {
+    if (numExercises > 1) {
+      removeExercise(exercise.id)
+
+      const nextExerciseFocus = exercises[idx - 1] || exercises[0]
+      const nextExerciseFocusID = nextExerciseFocus.id
+      const focusRef = test[nextExerciseFocusID]
+      if (nextExerciseFocusID && focusRef && focusRef.current) {
+        focusRef.current.focus()
+      }
+    }
+  }
+
   return (
     <div className='exercise-container'>
-      {/* <h5>Exercise {idx + 1}</h5> */}
       <FormInput
         val={name}
         setVal={setName}
         label={`Exercise ${idx + 1}`}
         LabelInfo={idx === 0 ? <InfoModal /> : null}
         placeholder='deadlifts 100 3x8'
+        inputID={exercise.id}
+        onEnter={handleEnter}
+        onBackspaceEmpty={handleBackspace}
+        nextID={nextID}
+        inputRef={inputRef}
       />
-      {/* <FormInput
-        val={data}
-        setVal={setData}
-        label='Weight, Sets x Reps'
-        placeholder='100 8x2, 7 / 90 8x3'
-      /> */}
     </div>
   )
 }
 
 type ExerciseType = {
+  id: string
   name: string
   sets: { reps: number | null; weight: number | null }[]
 }
 
-const initialExercise: ExerciseType = {
-  name: '',
-  sets: [{ reps: null, weight: null }],
+const generateNewExercise = (): ExerciseType => {
+  const id = uuidv4()
+  return {
+    id,
+    name: '',
+    sets: [{ reps: null, weight: null }],
+  }
 }
 
 const ExerciseList = () => {
-  const [exercises, setExercises] = useState<ExerciseType[]>([initialExercise])
+  const [exercises, setExercises] = useState<ExerciseType[]>([
+    generateNewExercise(),
+  ])
 
-  const handleAddExercise = () => {
-    setExercises(prev => [...prev, initialExercise])
+  const handleAddExercise = (): string => {
+    const newExercise = generateNewExercise()
+    setExercises(prev => [...prev, newExercise])
+    return newExercise.id
+  }
+  const handleRemoveExercise = (id: string) => {
+    setExercises(prev => prev.filter(exercise => exercise.id !== id))
   }
 
   return (
     <div className='exercise-list'>
-      {exercises.map((ex, idx) => {
-        return <ExerciseInputs exercise={ex} idx={idx} />
+      {exercises.map((ex, idx, exercises) => {
+        return (
+          <ExerciseInputs
+            exercise={ex}
+            idx={idx}
+            numExercises={exercises.length}
+            addExercise={handleAddExercise}
+            removeExercise={handleRemoveExercise}
+            exercises={exercises}
+          />
+        )
       })}
       <div className='add-exercise'>
         <button className='btn-no-styles' onClick={handleAddExercise}>
--- src/Components/FormInput/FormInput.tsx
+++ src/Components/FormInput/FormInput.tsx
@@ -8,6 +8,11 @@ type FormInputProps = {
   label: string
   LabelInfo?: React.ReactNode
   placeholder?: string
+  inputID?: string
+  onEnter?: (nextID: string | null) => void
+  onBackspaceEmpty?: () => void
+  nextID?: string | null
+  inputRef?: React.RefObject<HTMLInputElement>
 }
 
 const FormInput = ({
@@ -16,12 +21,15 @@ const FormInput = ({
   label,
   LabelInfo,
   placeholder,
+  inputID,
+  onEnter,
+  onBackspaceEmpty,
+  nextID = null,
+  inputRef,
 }: FormInputProps) => {
   const [isFocused, setIsFocused] = useState(false)
 
   const faded = val && !isFocused
-
-  const inputRef = useRef<HTMLInputElement>(null)
 
   // const focusInput = () => {
   //   if (inputRef.current) {
@@ -39,6 +47,16 @@ const FormInput = ({
     setVal('')
   }
 
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (onEnter && e.key === 'Enter') {
+      e.preventDefault()
+      onEnter(nextID)
+    } else if (!val && onBackspaceEmpty && e.key === 'Backspace') {
+      e.preventDefault()
+      onBackspaceEmpty()
+    }
+  }
+
   return (
     <div className='input-container'>
       <label className={faded ? 'fade' : ''}>
@@ -53,8 +71,10 @@ const FormInput = ({
           onChange={e => setVal(e.target.value)}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
+          onKeyDown={handleKeyPress}
           placeholder={placeholder}
           ref={inputRef}
+          id={inputID}
         />
         {isFocused && val ? (
           <button
