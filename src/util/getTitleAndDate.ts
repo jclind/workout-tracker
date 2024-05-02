6b7bf401dc234833097ee76a95926275b26d5311
Jesse Lind <Jesseclind@gmail.com>
2024-05-02 13:20:56 -0400

    fix: update getTitleAndDate function to give correct date for past year with shortened date

--- src/util/getTitleAndDate.ts
+++ src/util/getTitleAndDate.ts
@@ -9,9 +9,24 @@ export const getTitleAndDate = (
   )
   if (dateMatch) {
     const shortDateRegex = /^\d{1,2}\/\d{1,2}$/
+    const inputDate = new Date(Date.parse(dateMatch[1]))
+    const currentDate = new Date()
     if (shortDateRegex.test(dateMatch[1])) {
-      const dateWithCurrYear = `${dateMatch[1]}/${new Date().getFullYear()}`
-      formattedDate = new Date(Date.parse(dateWithCurrYear)).getTime()
+      // If short date, compare with current date
+      if (
+        inputDate.getMonth() > currentDate.getMonth() ||
+        (inputDate.getMonth() === currentDate.getMonth() &&
+          inputDate.getDate() > currentDate.getDate())
+      ) {
+        // If date is after current date, assume it's from the previous year
+        inputDate.setFullYear(currentDate.getFullYear() - 1)
+      } else {
+        // If date is on or before current date, use current year
+        inputDate.setFullYear(currentDate.getFullYear())
+      }
+      formattedDate = inputDate.getTime()
+      // const dateWithCurrYear = `${dateMatch[1]}/${new Date().getFullYear()}`
+      // formattedDate = new Date(Date.parse(dateWithCurrYear)).getTime()
     } else {
       formattedDate = new Date(Date.parse(dateMatch[1])).getTime()
     }
